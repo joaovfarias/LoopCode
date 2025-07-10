49132aa246c5e1a40bf9eadbb79b0151f9171693
Graziele <grazifagundesm@gmail.com>
2025-07-09 21:07:13 -0300

    add mui components

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,9 +1,19 @@
+import Checkbox from '@mui/material/Checkbox';
+
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
       <img src="/images/gato.jpg" alt="image" />
+
+      <div>
+      <Checkbox {...label} defaultChecked />
+      <Checkbox {...label} />
+      <Checkbox {...label} disabled />
+      <Checkbox {...label} disabled checked />
+    </div>
     </div>
   );
 }
