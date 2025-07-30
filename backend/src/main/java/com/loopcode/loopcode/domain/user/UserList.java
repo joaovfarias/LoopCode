bc4d66027a389bae4486fe73cc15787fefbf5c48
Miguel Botelho <135281223+Bot-e-lho@users.noreply.github.com>
2025-07-30 14:10:57 -0300

    Create UserList.java

---
+++ backend/src/main/java/com/loopcode/loopcode/domain/user/UserList.java
@@ -0,0 +1,29 @@
+package com.loopcode.loopcode.domain.user;
+
+import com.loopcode.loopcode.domain.exercise.Exercise;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "user_lists")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "username", nullable = false)
+    private User owner;
+
+    @ManyToMany
+    @JoinTable(name = "user_list_exercises", joinColumns = @JoinColumn(name = "list_id"), inverseJoinColumns = @JoinColumn(name = "exercise_id"))
+    private Set<Exercise> exercises;
+}
