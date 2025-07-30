363a6d3ea324608d42f801400c66644e12ea66b9
Miguel Botelho <135281223+Bot-e-lho@users.noreply.github.com>
2025-07-30 18:52:10 -0300

    fixing: user lists, upvote and downvote

--- backend/src/main/java/com/loopcode/loopcode/domain/challenge/ResolutionKey.java
+++ backend/src/main/java/com/loopcode/loopcode/domain/challenge/ResolutionKey.java
@@ -1,12 +1,16 @@
-package com.loopcode.loopcode.domain.challenge; // adjust package as needed
+package com.loopcode.loopcode.domain.challenge;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.io.Serializable;
 import java.time.LocalDate;
 
 @Embeddable
 @Data
+@NoArgsConstructor
+@AllArgsConstructor 
 public class ResolutionKey implements Serializable {
     private String username;
     private LocalDate challengeDate;
