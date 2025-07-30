bb6cf20bf3022dde3d17a6a0a52af9d9ee6883b0
Miguel Botelho <135281223+Bot-e-lho@users.noreply.github.com>
2025-07-30 18:56:06 -0300

    fixing: user lists, upvote and downvote

--- backend/src/main/java/com/loopcode/loopcode/repositories/ChallengeResolutionRepository.java
+++ backend/src/main/java/com/loopcode/loopcode/repositories/ChallengeResolutionRepository.java
@@ -2,9 +2,13 @@ package com.loopcode.loopcode.repositories;
 
 import com.loopcode.loopcode.domain.challenge.ChallengeResolution;
 import com.loopcode.loopcode.domain.challenge.ResolutionKey;
+
+import java.time.LocalDate;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChallengeResolutionRepository
         extends JpaRepository<ChallengeResolution, ResolutionKey> {
-    boolean existsByIdUsernameAndIdChallengeDate(String username, java.time.LocalDate date);
-} // talvez dê pra meter no dailychallengerepo
+
+    boolean existsByIdUsernameAndIdChallengeDate(String username, LocalDate challengeDate);
+}
